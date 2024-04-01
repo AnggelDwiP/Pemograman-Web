d946c9dbefb20d403b7bbc7c8d7ece83347fb061
Anggel Dwi Pramita <pramitaanggel@gmail.com>
2024-04-01 10:34:58 +0700

    PW08_Praktikum 4

--- pertemuan-8/form_upload_ajax.php
+++ pertemuan-8/form_upload_ajax.php
@@ -16,7 +16,7 @@
 </html> -->
 
 <!-- Soal 3.2 -->
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <title>Unggah Gambar</title>
@@ -30,5 +30,28 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="upload1.js"></script>
     </body>
-</html>
+</html> -->
 
+<!-- Praktikum 4 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Unggah File Dokumen</title>
+        <link rel="stylesheet" href="upload.css" type="text/css">
+    </head>
+    <body>
+        <div class="upload-form-container">
+            <h2>Unggah File Dokumen</h2>
+            <form action="upload_ajax.php" id="upload-form" method="post" enctype="multipart/form-data">
+                <div class="file-input-container">
+                    <input type="file" name="file" id="file" class="file-input">
+                    <label for="file" class="file-label">Pilih File</label>
+                </div>
+                <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+            </form>
+            <div id="status" class="upload-status"></div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="upload.js"></script>
+    </body>
+</html>
--- pertemuan-8/upload.js
+++ pertemuan-8/upload.js
@@ -20,10 +20,42 @@
 // });
 
 // Soal 3.2
-$(document).ready(function() {
-    $('#upload-form').submit(function(e) {
+// $(document).ready(function() {
+//     $('#upload-form').submit(function(e) {
+//         e.preventDefault();
+//         var formData = new FormData(this);
+//         $.ajax({
+//             type: 'POST',
+//             url: 'upload_ajax.php',
+//             data: formData,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function(response) {
+//                 $('#status').html(response);
+//             },
+//             error: function(xhr, status, error) {
+//                 $('#status').html('Terjadi kesalahan saat mengunggah file: ' + error);
+//             }
+//         });
+//     });
+// });
+
+// Praktikum 4
+$(document).ready(function(){
+    $('#file').change(function(){
+        if(this.files.length > 0){
+            $('#upload-button').prop('disabled', false).css('opacity',1);
+        }else{
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+    
+    $('#upload-form').submit(function(e){
         e.preventDefault();
+
         var formData = new FormData(this);
+
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
@@ -31,11 +63,11 @@ $(document).ready(function() {
             cache: false,
             contentType: false,
             processData: false,
-            success: function(response) {
+            success: function(response){
                 $('#status').html(response);
             },
-            error: function(xhr, status, error) {
-                $('#status').html('Terjadi kesalahan saat mengunggah file: ' + error);
+            error: function(){
+                $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });
     });
