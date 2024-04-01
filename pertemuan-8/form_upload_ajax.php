c45d0736664f43dc2e24805971f7671a01df0d77
Anggel Dwi Pramita <pramitaanggel@gmail.com>
2024-04-01 10:25:47 +0700

    PW08_Praktikum 3_jquery

---
+++ pertemuan-8/form_upload_ajax.php
@@ -0,0 +1,34 @@
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title>Unggah File Document</title>
+    </head>
+    <body>
+    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file" id="file">
+        <input type="submit" name="submit" value="Unggah">
+    </form>
+    <br>
+    <div id="status"></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="upload.js"></script>
+    </body>
+</html> -->
+
+<!-- Soal 3.2 -->
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title>Unggah Gambar</title>
+    </head>
+    <body>
+        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="files[]" id="files" multiple="multiple" accept=".jpg, .jpeg, .png, .gif">
+            <input type="submit" name="submit" value="Unggah">
+        </form>
+        <div id="status"></div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="upload1.js"></script>
+    </body>
+</html> -->
+
